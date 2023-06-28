dc86cff019384bd656ebc15affa2894ab4a83fae
Виктор Васильев <marcus.tv@yandex.ru>
2023-06-28 21:42:53 +0500

    Создан вывод меню при запуске игры и выход из игры

--- ConsolePRG/Program.cs
+++ ConsolePRG/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Menu();
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number == 0)
+                    break;
+            }
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine("\t---Главное меню---");
+            Console.WriteLine("1. Профиль");
+            Console.WriteLine("2. Битва");
+            Console.WriteLine("3. Магазин");
+            Console.WriteLine("0. Выход");
+            
         }
     }
 }
